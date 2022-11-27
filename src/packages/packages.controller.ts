e02aadabd20a566049282f10c8b5e5e98b61650a
Mohammad Tahvildary <shamot.group@gmail.com>
2022-11-27 12:53:52 +0330

    mock api created for finding packages

--- src/packages/packages.controller.ts
+++ src/packages/packages.controller.ts
@@ -1,8 +1,15 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { FindPackagesByProductDto } from './packages.dto';
+import { PackagesService } from './packages.service';
 
 @Controller('packages')
 export class PackagesController {
+  constructor(private readonly packagesService: PackagesService) {}
   @Get()
-  findPackagesByProducts(@Query() findPackages: FindPackagesByProductDto) {}
+  findPackagesByProducts(@Query() query: FindPackagesByProductDto) {
+    const packages = this.packagesService.findPackagesByProducts(query);
+    return {
+      packages,
+    };
+  }
 }
--- src/packages/packages.service.ts
+++ src/packages/packages.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { FindPackagesByProductDto } from './packages.dto';
 
 @Injectable()
-export class PackagesService {}
+export class PackagesService {
+  findPackagesByProducts(data: FindPackagesByProductDto) {
+    return [
+      {
+        asnid: 'ANSID',
+        package: {
+          url: 'https://shop.alteos.com/garantie/quote?category=laptop',
+          fee: {
+            amount: 89.4,
+            currency: 'euro',
+          },
+        },
+      },
+    ];
+  }
+}
